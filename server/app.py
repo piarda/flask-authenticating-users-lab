4fd8f49ab4ee063faa4c3f0b7954994b5f1e1572
piarda <matt.rindini@gmail.com>
2025-09-22 17:37:24 -0400

    final solution

--- server/app.py
+++ server/app.py
@@ -48,9 +48,40 @@ class ShowArticle(Resource):
 
         return {'message': 'Maximum pageview limit reached'}, 401
 
+class Login(Resource):
+    def post(self):
+        data = request.get_json()
+        username = data.get('username')
+
+        user = User.query.filter_by(username=username).first()
+        if user:
+            session['user_id'] = user.id
+            user_json = UserSchema().dump(user)
+            return user_json, 200
+        else:
+            return {}, 401
+
+class Logout(Resource):
+    def delete(self):
+        session.pop('user_id', None)
+        return '', 204
+
+class CheckSession(Resource):
+    def get(self):
+        user_id = session.get('user_id')
+        if user_id:
+            user = User.query.get(user_id)
+            if user:
+                user_json = UserSchema().dump(user)
+                return user_json, 200
+        return {}, 401
+
 api.add_resource(ClearSession, '/clear')
 api.add_resource(IndexArticle, '/articles')
 api.add_resource(ShowArticle, '/articles/<int:id>')
+api.add_resource(Login, '/login')
+api.add_resource(Logout, '/logout')
+api.add_resource(CheckSession, '/check_session')
 
 if __name__ == '__main__':
     app.run(port=5555, debug=True)
